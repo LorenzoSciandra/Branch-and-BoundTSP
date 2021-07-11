96911657280fcdef235062eb8a49243e239b6cf3
Stefano Vittorio Porta <stefa168@hotmail.it>
2021-07-11 18:19:32 +0200

    riformattazione

--- implementazione/src/tsp/BranchAndBound.java
+++ implementazione/src/tsp/BranchAndBound.java
@@ -13,7 +13,7 @@ public class BranchAndBound {
     public Graph<Integer, Integer, Integer> grafo;
     private Integer nodoCandidato;
 
-    public BranchAndBound(Graph<Integer, Integer, Integer> grafo,  Integer nodoCandidato) {
+    public BranchAndBound(Graph<Integer, Integer, Integer> grafo, Integer nodoCandidato) {
         this.grafo = grafo;
         SubProblem sp = new SubProblem(grafo, new ArrayList<>(), new ArrayList<>(), nodoCandidato);
         problemi = new ArrayList<>();
@@ -23,7 +23,7 @@ public class BranchAndBound {
 
     public CicloHamiltoniano branchAndBoundTsp() {
 
-        CicloHamiltoniano minimoCicloHamiltoniano = new CicloHamiltoniano(grafo,Integer.MAX_VALUE);
+        CicloHamiltoniano minimoCicloHamiltoniano = new CicloHamiltoniano(grafo, Integer.MAX_VALUE);
 
         while (!problemi.isEmpty()) {
             SubProblem problemaCorrente = problemi.get(0);
@@ -33,46 +33,42 @@ public class BranchAndBound {
                     minimoCicloHamiltoniano.setCosto(problemaCorrente.getLowerBound());
                     minimoCicloHamiltoniano.setGrafo(problemaCorrente.getUnoTree());
                 }
-
-            }
-            else{
+            } else {
                 if (problemaCorrente.isAmmissibile() && problemaCorrente.getLowerBound() < minimoCicloHamiltoniano.getCosto()) {
                     branching(problemaCorrente);
                 }
             }
-
         }
 
         return minimoCicloHamiltoniano;
     }
 
     private void branching(SubProblem problemaCorrente) {
-        List<Edge<Integer,Integer>> sottoCiclo = dfs(problemaCorrente.getUnoTree().getNode(nodoCandidato),new ArrayList<>());
+        List<Edge<Integer, Integer>> sottoCiclo = dfs(problemaCorrente.getUnoTree()
+                                                                      .getNode(nodoCandidato), new ArrayList<>());
 
-        ArrayList<Edge<Integer,Integer>> archiForzati = new ArrayList<>();
-        ArrayList<Edge<Integer,Integer>> archiVietati = new ArrayList<>();
+        ArrayList<Edge<Integer, Integer>> archiForzati = new ArrayList<>();
+        ArrayList<Edge<Integer, Integer>> archiVietati = new ArrayList<>();
 
-        for(int i = 0; i < sottoCiclo.size(); i++){
+        for (int i = 0; i < sottoCiclo.size(); i++) {
             archiVietati.add(sottoCiclo.get(i));
-            SubProblem sp = new SubProblem(grafo,archiVietati, archiForzati,nodoCandidato);
+            SubProblem sp = new SubProblem(grafo, archiVietati, archiForzati, nodoCandidato);
             problemi.add(sp);
             archiVietati.remove(0);
             archiForzati.add(sottoCiclo.get(i));
         }
-
     }
 
-    private List<Edge<Integer,Integer>> dfs(Node<Integer, Integer, Integer> nodoCorrente, ArrayList<Edge<Integer, Integer>> cicloInterno){
-        if (nodoCorrente.getKey().equals(nodoCandidato) && cicloInterno.size()>0){
+    private List<Edge<Integer, Integer>> dfs(Node<Integer, Integer, Integer> nodoCorrente,
+                                             ArrayList<Edge<Integer, Integer>> cicloInterno) {
+        if (nodoCorrente.getKey().equals(nodoCandidato) && cicloInterno.size() > 0) {
             return cicloInterno;
-        }
-        else{
-            for(Edge<Integer,Integer> arcoIncidente: nodoCorrente.getEdges()){
+        } else {
+            for (Edge<Integer, Integer> arcoIncidente : nodoCorrente.getEdges()) {
                 cicloInterno.add(arcoIncidente);
-                dfs(grafo.getNode(arcoIncidente.getTo()),cicloInterno);
+                dfs(grafo.getNode(arcoIncidente.getTo()), cicloInterno);
             }
             return cicloInterno;
         }
     }
-
 }
--- implementazione/src/tsp/SubProblem.java
+++ implementazione/src/tsp/SubProblem.java
@@ -4,7 +4,6 @@ import graph.MinimumSpanningTree;
 import graph.exceptions.GraphNodeMissingException;
 import graph.structures.Edge;
 import graph.structures.Graph;
-import graph.structures.Node;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -13,162 +12,149 @@ import java.util.Objects;
 
 public class SubProblem {
 
-  private Graph<Integer, Integer, Integer> grafoOriginale;
-  private List<Edge<Integer, Integer>> archiForzati;
-  private List<Edge<Integer, Integer>> archiVietati;
-  private Integer nodoCandidato;
-  private Graph<Integer,Integer,Integer> unoTree;
-  private int lowerBound;
-  private boolean haCicloHamiltoniano;
-  private boolean ammissibile;
+    private Graph<Integer, Integer, Integer> grafoOriginale;
+    private List<Edge<Integer, Integer>> archiForzati;
+    private List<Edge<Integer, Integer>> archiVietati;
+    private Integer nodoCandidato;
+    private Graph<Integer, Integer, Integer> unoTree;
+    private int lowerBound;
+    private boolean haCicloHamiltoniano;
+    private boolean ammissibile;
 
-  public SubProblem(Graph<Integer, Integer, Integer> grafoOriginale,
-                    ArrayList<Edge<Integer, Integer>> archiForzati,
-                    ArrayList<Edge<Integer, Integer>> archiVietati,
-                    Integer nodoCandidato) {
-      this.archiForzati = archiForzati;
-      this.archiVietati = archiVietati;
-      this.grafoOriginale = grafoOriginale;
-      this.nodoCandidato = nodoCandidato;
-      this.unoTree = calcola1Tree();
-      this.lowerBound = costo1Tree();
-      this.haCicloHamiltoniano = controllaCiclo();
-      this.ammissibile = unoTree.getNodes().size() == grafoOriginale.getNodes().size() &&
-                         unoTree.getEdges().size() == grafoOriginale.getNodes().size();
-  }
-
-
-    private class ComparatorIntegerEdge implements Comparator<Edge<Integer, Integer>>{
-        @Override
-        public int compare(Edge<Integer, Integer> o1, Edge<Integer, Integer> o2) {
-            return o1.getLabel().compareTo(o2.getLabel());
-        }
+    public SubProblem(Graph<Integer, Integer, Integer> grafoOriginale,
+                      ArrayList<Edge<Integer, Integer>> archiForzati,
+                      ArrayList<Edge<Integer, Integer>> archiVietati,
+                      Integer nodoCandidato) {
+        this.archiForzati = archiForzati;
+        this.archiVietati = archiVietati;
+        this.grafoOriginale = grafoOriginale;
+        this.nodoCandidato = nodoCandidato;
+        this.unoTree = calcola1Tree();
+        this.lowerBound = costo1Tree();
+        this.haCicloHamiltoniano = controllaCiclo();
+        this.ammissibile = unoTree.getNodes().size() == grafoOriginale.getNodes().size() &&
+                           unoTree.getEdges().size() == grafoOriginale.getNodes().size();
     }
 
-  // evaluate
-  private Graph<Integer,Integer,Integer> calcola1Tree (){
-      Graph<Integer,Integer,Integer> grafoPerMST = grafoOriginale.clone();
-      grafoPerMST.removeNode(nodoCandidato);
+    // evaluate
+    private Graph<Integer, Integer, Integer> calcola1Tree() {
+        Graph<Integer, Integer, Integer> grafoPerMST = grafoOriginale.clone();
+        grafoPerMST.removeNode(nodoCandidato);
 
-      Graph<Integer,Integer,Integer> mst = MinimumSpanningTree.kruskalForTSP(grafoPerMST, new ComparatorIntegerEdge(),archiForzati, archiVietati);
-      mst.addNode(nodoCandidato);
+        Graph<Integer, Integer, Integer> mst = MinimumSpanningTree.kruskalForTSP(grafoPerMST,
+                                                                                 new ComparatorIntegerEdge(),
+                                                                                 archiForzati, archiVietati);
+        mst.addNode(nodoCandidato);
 
-      ArrayList<Edge<Integer,Integer>> archiIncidentiForzatiCandidato = new ArrayList<>();
-      for (Edge<Integer,Integer> incidenteForzato: archiForzati){
-          if(Objects.equals(incidenteForzato.getFrom(), nodoCandidato) || Objects.equals(incidenteForzato.getTo(), nodoCandidato)){
-              archiIncidentiForzatiCandidato.add(incidenteForzato);
-          }
-      }
-
-      ArrayList<Edge<Integer,Integer>> archiIncidentiVietatiCandidato = new ArrayList<>();
-      for (Edge<Integer,Integer> incidenteVietato: archiVietati){
-          if(Objects.equals(incidenteVietato.getFrom(), nodoCandidato) || Objects.equals(incidenteVietato.getTo(), nodoCandidato)){
-              archiIncidentiVietatiCandidato.add(incidenteVietato);
-          }
-      }
-
-      Edge<Integer,Integer> primoArcoTrovato = null;
-      Edge<Integer,Integer> secondoArcoTrovato = null;
-      if(archiIncidentiForzatiCandidato.size()>=2) {
-        for(Edge<Integer,Integer> e: archiIncidentiForzatiCandidato){
-            if(primoArcoTrovato == null){
-                primoArcoTrovato = e;
+        ArrayList<Edge<Integer, Integer>> archiIncidentiForzatiCandidato = new ArrayList<>();
+        for (Edge<Integer, Integer> incidenteForzato : archiForzati) {
+            if (Objects.equals(incidenteForzato.getFrom(), nodoCandidato) || Objects.equals(incidenteForzato.getTo(),
+                                                                                            nodoCandidato)) {
+                archiIncidentiForzatiCandidato.add(incidenteForzato);
             }
-            else if(secondoArcoTrovato == null){
-                secondoArcoTrovato = e;
+        }
+
+        ArrayList<Edge<Integer, Integer>> archiIncidentiVietatiCandidato = new ArrayList<>();
+        for (Edge<Integer, Integer> incidenteVietato : archiVietati) {
+            if (Objects.equals(incidenteVietato.getFrom(), nodoCandidato) || Objects.equals(incidenteVietato.getTo(),
+                                                                                            nodoCandidato)) {
+                archiIncidentiVietatiCandidato.add(incidenteVietato);
             }
-            else{
-                if(primoArcoTrovato.getLabel() < secondoArcoTrovato.getLabel()){
-                    if(e.getLabel() < secondoArcoTrovato.getLabel()){
-                        secondoArcoTrovato = e;
+        }
+
+        Edge<Integer, Integer> primoArcoTrovato = null;
+        Edge<Integer, Integer> secondoArcoTrovato = null;
+        if (archiIncidentiForzatiCandidato.size() >= 2) {
+            for (Edge<Integer, Integer> e : archiIncidentiForzatiCandidato) {
+                if (primoArcoTrovato == null) {
+                    primoArcoTrovato = e;
+                } else if (secondoArcoTrovato == null) {
+                    secondoArcoTrovato = e;
+                } else {
+                    if (primoArcoTrovato.getLabel() < secondoArcoTrovato.getLabel()) {
+                        if (e.getLabel() < secondoArcoTrovato.getLabel()) {
+                            secondoArcoTrovato = e;
+                        }
+                    } else {
+                        if (e.getLabel() < primoArcoTrovato.getLabel()) {
+                            primoArcoTrovato = e;
+                        }
                     }
                 }
-                else{
-                    if(e.getLabel() < primoArcoTrovato.getLabel()){
-                        primoArcoTrovato = e;
+            }
+
+        } else if (archiIncidentiForzatiCandidato.size() == 1) {
+            primoArcoTrovato = archiIncidentiForzatiCandidato.get(0);
+
+            for (Edge<Integer, Integer> arcoDaAggiungere : grafoOriginale.getEdges()) {
+                if (!archiIncidentiVietatiCandidato.contains(arcoDaAggiungere)) {
+                    if (Objects.equals(arcoDaAggiungere.getTo(), nodoCandidato) || Objects.equals(arcoDaAggiungere.getFrom(), nodoCandidato)) {
+                        if (secondoArcoTrovato == null) {
+                            secondoArcoTrovato = arcoDaAggiungere;
+                        } else {
+                            if (arcoDaAggiungere.getLabel() < secondoArcoTrovato.getLabel()) {
+                                secondoArcoTrovato = arcoDaAggiungere;
+                            }
+                        }
+                    }
+                }
+            }
+
+        } else {
+            for (Edge<Integer, Integer> arcoDaAggiungere : grafoOriginale.getEdges()) {
+                if (!archiIncidentiVietatiCandidato.contains(arcoDaAggiungere)) {
+                    if (Objects.equals(arcoDaAggiungere.getTo(), nodoCandidato) || Objects.equals(arcoDaAggiungere.getFrom(), nodoCandidato)) {
+                        if (primoArcoTrovato == null) {
+                            primoArcoTrovato = arcoDaAggiungere;
+                        } else if (secondoArcoTrovato == null) {
+                            secondoArcoTrovato = arcoDaAggiungere;
+                        } else {
+                            if (primoArcoTrovato.getLabel() < secondoArcoTrovato.getLabel()) {
+                                if (arcoDaAggiungere.getLabel() < secondoArcoTrovato.getLabel()) {
+                                    secondoArcoTrovato = arcoDaAggiungere;
+                                }
+                            } else {
+                                if (arcoDaAggiungere.getLabel() < primoArcoTrovato.getLabel()) {
+                                    primoArcoTrovato = arcoDaAggiungere;
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
 
-      }
-      else if(archiIncidentiForzatiCandidato.size()==1){
-          primoArcoTrovato = archiIncidentiForzatiCandidato.get(0);
 
-          for(Edge<Integer,Integer> arcoDaAggiungere: grafoOriginale.getEdges()){
-              if(!archiIncidentiVietatiCandidato.contains(arcoDaAggiungere)){
-                  if(Objects.equals(arcoDaAggiungere.getTo(), nodoCandidato) || Objects.equals(arcoDaAggiungere.getFrom(), nodoCandidato)){
-                      if(secondoArcoTrovato == null){
-                          secondoArcoTrovato = arcoDaAggiungere;
-                      }
-                      else {
-                          if(arcoDaAggiungere.getLabel() < secondoArcoTrovato.getLabel()){
-                              secondoArcoTrovato = arcoDaAggiungere;
-                          }
-                      }
-                  }
-              }
-          }
-
-      }
-      else{
-          for(Edge<Integer,Integer> arcoDaAggiungere: grafoOriginale.getEdges()){
-              if(!archiIncidentiVietatiCandidato.contains(arcoDaAggiungere)){
-                  if(Objects.equals(arcoDaAggiungere.getTo(), nodoCandidato) || Objects.equals(arcoDaAggiungere.getFrom(), nodoCandidato)) {
-                      if(primoArcoTrovato == null){
-                          primoArcoTrovato = arcoDaAggiungere;
-                      }
-                      else if(secondoArcoTrovato == null){
-                          secondoArcoTrovato = arcoDaAggiungere;
-                      }
-                      else{
-                          if(primoArcoTrovato.getLabel() < secondoArcoTrovato.getLabel()){
-                              if(arcoDaAggiungere.getLabel() < secondoArcoTrovato.getLabel()){
-                                  secondoArcoTrovato = arcoDaAggiungere;
-                              }
-                          }
-                          else{
-                              if(arcoDaAggiungere.getLabel() < primoArcoTrovato.getLabel()){
-                                  primoArcoTrovato = arcoDaAggiungere;
-                              }
-                          }
-                      }
-                  }
-              }
-          }
-      }
+        if (primoArcoTrovato != null && secondoArcoTrovato != null) {
+            try {
+                mst.addEdge(primoArcoTrovato.getFrom(), secondoArcoTrovato.getTo(), secondoArcoTrovato.getLabel());
+                mst.addEdge(secondoArcoTrovato.getFrom(), secondoArcoTrovato.getTo(), secondoArcoTrovato.getLabel());
+            } catch (GraphNodeMissingException e) {
+                e.printStackTrace();
+            }
+        }
 
 
-      if(primoArcoTrovato!=null && secondoArcoTrovato!=null){
-          try {
-              mst.addEdge(primoArcoTrovato.getFrom(),secondoArcoTrovato.getTo(),secondoArcoTrovato.getLabel());
-              mst.addEdge(secondoArcoTrovato.getFrom(),secondoArcoTrovato.getTo(),secondoArcoTrovato.getLabel());
-          } catch (GraphNodeMissingException e) {
-              e.printStackTrace();
-          }
-      }
-
-
-      return mst;
-  }
+        return mst;
+    }
 
     private int costo1Tree() {
-      int costo = 0;
+        int costo = 0;
 
-      for(Edge<Integer,Integer> arco: this.unoTree.getEdges()){
-          costo = costo + arco.getLabel();
-      }
-      return costo;
+        for (Edge<Integer, Integer> arco : this.unoTree.getEdges()) {
+            costo = costo + arco.getLabel();
+        }
+        return costo;
     }
 
-    private boolean controllaCiclo(){
-      boolean controllo = true;
+    private boolean controllaCiclo() {
+        boolean controllo = true;
 
-      for(int i = 0; i < unoTree.getNodes().size() && controllo; i++){
-          controllo = unoTree.getNodes().get(i).getEdges().size() == 2;
-      }
+        for (int i = 0; i < unoTree.getNodes().size() && controllo; i++) {
+            controllo = unoTree.getNodes().get(i).getEdges().size() == 2;
+        }
 
-      return controllo;
+        return controllo;
     }
 
     public Graph<Integer, Integer, Integer> getGrafoOriginale() {
@@ -201,5 +187,12 @@ public class SubProblem {
 
     public boolean isAmmissibile() {
         return ammissibile;
+    }
+
+    private class ComparatorIntegerEdge implements Comparator<Edge<Integer, Integer>> {
+        @Override
+        public int compare(Edge<Integer, Integer> o1, Edge<Integer, Integer> o2) {
+            return o1.getLabel().compareTo(o2.getLabel());
+        }
     }
 }
