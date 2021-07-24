707a90c03c851ff406f6105714826b4c5694c968
Stefa168 <stefa168@hotmail.it>
2021-07-24 18:09:58 +0200

    Multiple migliorie. Aggiunto un parametro per saltare un certo numero di grafi prima di iniziare con la computazione Rimosso il timer della computazione per utilizzare quello presente in TSPResult Aggiunto un beep quando i test terminano

--- implementazione/src/StatsCalculator.java
+++ implementazione/src/StatsCalculator.java
@@ -1,11 +1,14 @@
 import graph.exceptions.GraphNodeMissingException;
-import graph.structures.Graph;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import tsp.BranchAndBound;
 import tsp.TSPResult;
 
-import java.util.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 import java.util.concurrent.*;
 import java.util.function.BiFunction;
 
@@ -20,17 +23,21 @@ public class StatsCalculator {
         int edgeMaxValue = Integer.parseInt(args[3]);
         int maxExecutionTime = Integer.parseInt(args[4]);
         int repeatsForNodeCount = Integer.parseInt(args[5]);
+
         Random random;
         long seed;
-
         if (args.length < 7) {
             seed = System.currentTimeMillis();
             logger.info("Using seed {} as it hasn't been specified.", seed);
-            random = new Random(seed);
         } else {
-            seed = Integer.parseInt(args[6]);
-            logger.info("Using seed {} .", seed);
-            random = new Random(seed);
+            seed = Long.parseLong(args[6]);
+            logger.info("Using seed {}", seed);
+        }
+        random = new Random(seed);
+
+        int skipFirstNGraphs = 0;
+        if (args.length >= 8) {
+            skipFirstNGraphs = Integer.parseInt(args[7]);
         }
 
         ExecutorService service = Executors.newSingleThreadExecutor();
@@ -41,6 +48,13 @@ public class StatsCalculator {
             logger.info("Testing with {} nodes", nodeCount);
 
             ArrayList<BranchAndBound> graphs = new ArrayList<>();
+
+            for (int i = 0; i < skipFirstNGraphs; i++) {
+                BasicCompleteGraphGenerator.generateCompleteGraph(nodeCount,
+                                                                  edgeMinValue,
+                                                                  edgeMaxValue,
+                                                                  random);
+            }
 
             for (int i = 0; i < repeatsForNodeCount; i++) {
                 BranchAndBound bnb = new BranchAndBound(BasicCompleteGraphGenerator.generateCompleteGraph(nodeCount,
@@ -64,33 +78,28 @@ public class StatsCalculator {
                 for (int i = 0; i < graphs.size(); i++) {
                     BranchAndBound branchAndBound = graphs.get(i);
 
-
-                    long time1 = System.currentTimeMillis();
                     Future<TSPResult> future = service.submit(() -> branchAndBound.solveProblem(threadCount));
 
                     try {
                         TSPResult result = future.get(maxExecutionTime, TimeUnit.SECONDS);
-                        long time = System.currentTimeMillis() - time1;
+                        Optional<Long> time = result.getComputationTime();
 
                         logger.info("{} Nodes, {} Threads (Run {}/{}) completed after {} milliseconds",
                                     nodeCount,
                                     threadCount,
-                                    i, repeatsForNodeCount,
-                                    time);
-/*                    logger.info(result);
-                    logger.info("Created: {}; Interm: {}; Viable: {}; Bound: {}; Unfeas: {}",
-                                result.getTotalNodesCount(),
-                                result.getIntermediateNodesCount(),
-                                result.getClosedNodesForBestCount(),
-                                result.getClosedNodesForBound(),
-                                result.getClosedNodesForUnfeasibilityCount());*/
+                                    i + 1, repeatsForNodeCount,
+                                    time.orElseThrow());
 
-                        threadResults[j].addValue(time);
+                        threadResults[j].addTime(time.orElseThrow());
+                        threadResults[j].addGeneratedNodes(result.getTotalNodesCount());
+
                     } catch (InterruptedException | ExecutionException e) {
                         logger.error(e);
                     } catch (TimeoutException e) {
                         logger.warn("Timeout reached. There might be an exception right above or below ↕");
                         future.cancel(true);
+                    } finally {
+                        System.gc();
                     }
                 }
             }
@@ -112,20 +121,24 @@ public class StatsCalculator {
             logger.info("=================================================================");
             logger.info("Results for {} Nodes ({} Edges)", statBlock[0].nodeCount, statBlock[0].edgeCount);
             for (BnBStats threadStats : statBlock) {
-                logger.info("{} Threads - Average: {}ms ({} / {}); Best: {}ms; Worst: {}ms.",
+                logger.info("{} Threads - Average: {}ms ({} / {}); Best: {}ms; Worst: {}ms; Nodes: {}",
                             threadStats.getThreads(),
                             threadStats.getAverageTime().getAverage(),
                             threadStats.getAverageTime().getCount(),
                             repeatsForNodeCount,
                             threadStats.getBestTimeString(),
-                            threadStats.getWorstTimeString());
+                            threadStats.getWorstTimeString(),
+                            threadStats.getAverageNodes().getAverage());
             }
         }
+
+        Toolkit.getDefaultToolkit().beep();
     }
 
     public static class BnBStats {
         private final long nodeCount, edgeCount, threads;
-        private final AverageTime averageTime = new AverageTime();
+        private final AverageAccumulator averageTime = new AverageAccumulator();
+        private final AverageAccumulator averageNodes = new AverageAccumulator();
         private final ConditionalTime bestTime = new ConditionalTime((nv, ov) -> nv < ov);
         private final ConditionalTime worstTime = new ConditionalTime((nv, ov) -> nv > ov);
 
@@ -135,10 +148,14 @@ public class StatsCalculator {
             this.threads = threads;
         }
 
-        public void addValue(long value) {
+        public void addTime(long value) {
             averageTime.addValue(value);
             bestTime.tryUpdateValue(value);
             worstTime.tryUpdateValue(value);
+        }
+
+        public void addGeneratedNodes(int nodes) {
+            averageNodes.addValue(nodes);
         }
 
         public long getNodeCount() {
@@ -153,8 +170,12 @@ public class StatsCalculator {
             return threads;
         }
 
-        public AverageTime getAverageTime() {
+        public AverageAccumulator getAverageTime() {
             return averageTime;
+        }
+
+        public AverageAccumulator getAverageNodes() {
+            return averageNodes;
         }
 
         public Long getBestTime() {
@@ -172,7 +193,6 @@ public class StatsCalculator {
         public String getWorstTimeString() {
             return worstTime.getValue() == null ? "ND" : worstTime.getValue().toString();
         }
-
     }
 
     public static class ConditionalTime {
@@ -197,10 +217,10 @@ public class StatsCalculator {
         }
     }
 
-    public static class AverageTime {
+    public static class AverageAccumulator {
         private long time, count;
 
-        public AverageTime() {
+        public AverageAccumulator() {
             this.time = 0;
             this.count = 0;
         }
